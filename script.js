64e73833b67bf69c79768915f663613865631bcd
1zezopinto <g4123ms@gmail.com>
2024-08-29 17:30:01 -0300

    mudança na estilização

--- script.js
+++ script.js
@@ -11,3 +11,54 @@ const myObserver = new IntersectionObserver((entries) => {
 const elements = document.querySelectorAll('.hidden')
 
 elements.forEach((element) => myObserver.observe(element))
+
+// contato
+
+document.addEventListener('DOMContentLoaded', function () {
+    const options = document.querySelectorAll('.input');
+    const textBox = document.getElementById('text-box');
+
+    const texts = {
+        option1: '<a href="https://wa.me/93991157295">(93) 99115-7295</a>',
+        option2: '<a href="https://www.instagram.com/yle.systems" target="_blank">@yle.systems</a>',
+        option3: '<a href="mailto:yle.contato@gmail.com">yle.contato@gmail.com</a>'
+    };
+
+    function updateText() {
+        const selectedOption = document.querySelector('.input:checked').value;
+        textBox.innerHTML = texts[selectedOption]; // Use innerHTML para incluir HTML
+        textBox.style.display = 'block';
+    }
+
+    options.forEach(option => {
+        option.addEventListener('change', updateText);
+    });
+
+    // Inicializa o texto exibido
+    updateText();
+});
+
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     const options = document.querySelectorAll('.input');
+//     const textBox = document.getElementById('text-box');
+
+//     const texts = {
+//         option1: "Telefone: (93) 99115-7295",
+//         option2: "Instagram: @yle.systems",
+//         option3: "E-mail: yle.contato@gmail.com"
+//     };
+
+//     function updateText() {
+//         const selectedOption = document.querySelector('.input:checked').value;
+//         textBox.textContent = texts[selectedOption];
+//         textBox.style.display = 'block';
+//     }
+
+//     options.forEach(option => {
+//         option.addEventListener('change', updateText);
+//     });
+
+//     // Inicializa o texto exibido
+//     updateText();
+// });
